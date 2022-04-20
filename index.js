eeabdc63d722d986a23f58c5cc36643cdf08d56e
Umesh Singh Punia <punia.umesh@gmail.com>
2022-04-20 17:59:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+// var a=1
+// b=2
+// document.write(a+''+b)
+// document.write(a+b) concat
+// using 3rd variable
+
+// c=a
+// a=b
+// b=c
+
+// without using 3rd
+// a=a+b //a=1+2=3
+// b=a-b //b=3-2=1
+// a=a-b //a=3-1=2
+
+// single line
+// a=(a+b)-(b=a)
+
+// a=(1+2)-1
+
+
+
+// document.write(10!=="10")
+
+
+// a=1
+// document.write(true==1)
+
+
+
+
+// document.write(a)
+
+
+
+
