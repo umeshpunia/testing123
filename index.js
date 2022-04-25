c0dd465c2e48c5ffd15d7bd6e8823f04947d037a
Umesh Singh Punia <punia.umesh@gmail.com>
2022-04-25 18:06:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -113,6 +113,89 @@
 
 
 
+// for(i=1;i<=5;i++){
+//     for(j=1;j<=5;j++){
+//         if(i==1 ){
+//             document.write("*")
+//         }else if(j==1){
+//             document.write("*")
+//         }
+//         else if(i==5 && j<4){
+//             document.write("*")
+//         }
+//         if(i==3 && (j==3 || j==4) ){
+//             document.write("*")
+//         }else if(i==4 && j==4){
+//             document.write("*")
+//         }
+//         else{
+//             document.write("&nbsp;&nbsp;")
+//         }
+//     }
+//     document.write("<br>")
+// }
+
+
+
+
+// here b and c is parameters
+// function a(b,c){
+//     document.write(b+c+"<br>")
+    
+// }
+// here 2,3 are arguments
+// a(2,3)
+
+// a(6,9)
+
+
+// a('hello all')
+
+// a()
+
+// function a(){
+//     document.write(5)
+//     return 6
+// }
+// document.write(a()+60)
+
+// a()
+// anonymous
+// var a=function(){
+//     document.write("Hello")
+// }
+// task 1
+// a=[1,3,6]
+
+
+
+
+// a=["jkdjkfs",6,true]
+// document.write(a[2])
+// console.log(a)
+
+// for(i=0;i<=2;i++){
+//     document.write(a[i]+"<br>")
+// }
+// a={name:'umesh',city:'noida'}
+
+// console.log(a)
+// document.write(a.city+a.name)
+
+// document.write(a["name"])
+
+
+
+// JSON -> Java Script Object Notation
+// API
+
+
+
+
+
+
+
+
 
 
 
