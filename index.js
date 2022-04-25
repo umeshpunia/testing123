83664d22d19bb46c1f364f95e154b31bb0ce795b
Umesh Singh Punia <punia.umesh@gmail.com>
2022-04-25 10:38:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,18 +18,114 @@
 
 // a=(1+2)-1
 
-
-
 // document.write(10!=="10")
-
 
 // a=1
 // document.write(true==1)
 
-
-
-
 // document.write(a)
+
+// if(a>b){
+//     document.write('A is greater')
+// }else if(b>a){
+//     document.write('B is greater')
+// }else{
+//     document.write('Equal')
+// }
+
+// a = 200;
+// b = 200;
+// switch (false) {
+//   case a > b: //false
+//     document.write("a");
+//     break;
+//   case b > a: //false
+//     document.write("b");
+//     break
+//   default:
+//     document.write("==");
+// }
+
+
+
+
+// day=new Date().getDay()
+
+// switch(day){
+//     case 1:
+//         document.write('Monday')
+//         break;
+//         case 4:
+//         document.write('Thursday')
+//         break;
+// }
+
+
+
+// 10 < 20
+
+
+
+// for(a=10;a<21;a++){
+
+//     if(a==15){
+//        continue
+//     }
+//     document.write(a+"<br>")
+// }
+
+
+
+
+
+
+
+
+// 75 > 55
+
+// a=75;
+// do{
+   
+//     document.write(a+"<br>")
+//     a--
+// }while(a<=55)
+
+
+
+
+
+
+
+// number=10
+// for(i=1;i<=10;i++){
+//     document.write(number+"*"+i+"="+number*i+"<br>")
+// }
+
+// 2*1=2
+
+// // 
+// -----
+// -   -
+// -----
+// -   -
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
