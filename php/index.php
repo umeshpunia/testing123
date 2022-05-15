824f04b9d716e5279a9889da5d66ba5b70aafd51
Umesh Singh Punia <punia.umesh@gmail.com>
2022-05-15 11:04:51 +0530

    Add files via upload

---
+++ php/index.php
@@ -0,0 +1,74 @@
+<?php
+
+// $a=false;
+
+// echo $a;
+
+
+/*
+
+    array
+    1. indexed
+        single
+        multi
+    2. associative
+
+        it has a key and value pair
+
+
+        ["key1"=>value1,"key2"=>value2,......]
+
+*/
+
+
+
+
+// print_r($a["name"]);
+
+// echo end($a);
+
+
+
+// $a=["name"=>"umesh","city"=>"delhi"];
+
+// // foreach
+
+// foreach($a as $x=>$v){
+//     echo $x."====".$v ."<br>";
+// }
+
+
+
+// $a=["abc","name"=>["xyz","45455dadasd","z"=>["8iujkdsfsh"]]];
+
+// print_r($a);
+
+// echo $a["name"]["z"][0];
+
+echo "<pre>";
+print_r($_SERVER);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
